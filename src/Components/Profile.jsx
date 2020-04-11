ec87d1a164d27a3998fd241cda96ff5c0aa00610
kok1m <co.kimblessed@gmail.com>
2020-04-11 11:34:40 +0600

    Added all files

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img alt='111' src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"/>
+      </div>
+      <div>
+        Avatar + description
+      </div>
+      <div>
+        My Posts
+        <div>
+          New post
+        </div>
+      </div>
+      <div>
+        Post1
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
